aea9cf2e40af42e1f826f6f5d31b238a21f59d7e
robin123 <robinmahmud358@gmail.com>
2026-02-02 17:14:06 +0600

    card fuction implement

--- login.js
+++ login.js
@@ -1,21 +1,17 @@
-document.getElementById('login-btn').addEventListener('click',(event)=>{
+document.getElementById('login-btn').addEventListener('click', (event) => {
     event.preventDefault();
-    const accountNumber=document.getElementById("account-number").value;
-    const pinNumber=document.getElementById("pin-number").value;
-    const pinIntiger=parseInt(pinNumber);
-    if(accountNumber.length===11)
-    {
-        if(pinIntiger==1234)
-        {
-            window.location.href="./main.html";
+    const accountNumber = document.getElementById("account-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    const pinIntiger = parseInt(pinNumber);
+    if (accountNumber.length === 11) {
+        if (pinIntiger == 1234) {
+            window.location.href = "./main.html";
         }
-        else
-        {
+        else {
             alert('Your Pin Number is not Valid')
         }
-
     }
-    else{
+    else {
         alert("Your Account Number is not valid");
     }
 })
